d4ad84a64fef064fcb591ae4e2ccc2b49bdcb803
Jacob Francois <jafranswa@gmail.com>
2017-11-30 12:32:35 -0600

    finished 3 TODOs outlined for assignment

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -22,6 +22,23 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    // TODONE #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs;
+
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("list", JobData.findByValue(searchTerm));
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs;
+
+            model.addAttribute("columns", ListController.columnChoices);
+            model.addAttribute("list", JobData.findByColumnAndValue(searchType, searchTerm));
+            return "search";
+        }
+    }
 }
+
